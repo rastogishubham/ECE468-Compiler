4ed0dcb3bd6d8426516c02a3d8d5c5275a21b66e
Shubham Sandeep Rastogi <rastogis@ecegrid-thin7.ecn.purdue.edu>
2016-09-05 02:15:08 -0400

    put all files in a step 1 folder

---
+++ step1code/build/MicroListener.java
@@ -0,0 +1,19 @@
+// Generated from Micro.g4 by ANTLR 4.5.3
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link MicroParser}.
+ */
+public interface MicroListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link MicroParser#dummy}.
+	 * @param ctx the parse tree
+	 */
+	void enterDummy(MicroParser.DummyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MicroParser#dummy}.
+	 * @param ctx the parse tree
+	 */
+	void exitDummy(MicroParser.DummyContext ctx);
+}
