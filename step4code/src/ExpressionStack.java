bcfda569f46efc8267436382d5cfe334d854ef9e
Shubham Sandeep Rastogi <rastogis@cparch13.ecn.purdue.edu>
2016-10-20 22:02:50 -0400

    Removed unnecessary print statements

--- step4code/src/ExpressionStack.java
+++ step4code/src/ExpressionStack.java
@@ -12,30 +12,23 @@ public class ExpressionStack {
         this.createOperatorTable();
         for(int i = 0; i < expression.length(); i++) {
             char token = expression.charAt(i);
-            //System.out.println("token " + token);
             if(token != ')' && token != '(') {
                 this.compareOperators(Character.toString(token));
             }
             else if(token == '(') {
                 this.operatorStack.push(Character.toString(token));
-                //System.out.println("1 Pushed " + token + " on the stack");
             }
             else {
                 String currOperator = this.operatorStack.pop();
-                //System.out.println("1 Popped " + currOperator + " off the stack");
                 while(!currOperator.equals("(")) {
                     this.postFixExpr += currOperator;
-                  //  System.out.println("1 postFixExpr "+ this.postFixExpr);
                     currOperator = this.operatorStack.pop();
-                 //   System.out.println("2 Popped " + currOperator + " off the stack");
                 }
             }      
         }
         while(!operatorStack.isEmpty()) {
             String val = (String) operatorStack.pop();
-            //System.out.println("3 Popped" + val + " off the stack");
             this.postFixExpr += val;
-            //System.out.println("1 postFixExpr "+ this.postFixExpr);
         }
         return this.postFixExpr;
     }
@@ -54,12 +47,9 @@ public class ExpressionStack {
         if(this.OperatorTable.containsKey(token) && !this.operatorStack.isEmpty()) {
             currPrecedence = this.OperatorTable.get(token);
             stackOperator = (String) this.operatorStack.pop();
-            //System.out.println("4 Popped " + stackOperator + " off the stack");
             if(stackOperator.equals("(")) {
                 this.operatorStack.push(stackOperator);
-               // System.out.println("2 Pushed " + stackOperator + " on the stack");
                 this.operatorStack.push(token);
-              //  System.out.println("3 Pushed " + token + " on the stack");
             }
             else {
                 stackPrecedence = this.OperatorTable.get(stackOperator);
